e7cf65c128fc4e4b054534b4800aca61bee22ed1
TnTora <128092613+TnTora@users.noreply.github.com>
2026-04-15 06:04:34 +0200

    add epub reader route (wip)

---
+++ src/frontend/src/lib/components/BookRenderer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let xhtml_content: string;
+
+</script>
+
+<svelte:head>
+    <link href="../style/book-style.css" rel="stylesheet" type="text/css"/>
+
+</svelte:head>
+
+<div class="overflow-scroll! w-full! h-screen! px-4! bg-neutral-800!" style="column-width:40vw; column-gap: 3rem;">
+    {@html xhtml_content}
+</div>
---
+++ src/frontend/src/routes/books/[id]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import BookRenderer from "$lib/components/BookRenderer.svelte";
+    let { params } = $props();
+    console.log(params.id);
+</script>
+
+<BookRenderer />
