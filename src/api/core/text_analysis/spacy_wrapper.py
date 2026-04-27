825823fd858d386a521694a7e4576d5ca31afc6f
TnTora <128092613+TnTora@users.noreply.github.com>
2026-04-27 13:04:33 +0200

    add spacy blank model and filtered parse

--- src/api/core/text_analysis/spacy_wrapper.py
+++ src/api/core/text_analysis/spacy_wrapper.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable, Iterator
 import spacy
 from spacy.cli import download
 from api.schemas.core import Morpheme
@@ -12,11 +13,14 @@ class SpacyAnalyzer:
     # TODO: Test creating tokenizers for each thread
     LOCK: threading.Lock = threading.Lock()
 
-    def __init__(self, model_name:str) -> None:
+    def __init__(self, model_name: str, blank: bool = False) -> None:
         self.model_name = model_name
-        self.nlp = spacy.load(model_name)
+        if blank:
+            self.nlp = spacy.blank(model_name)
+        else:
+            self.nlp = spacy.load(model_name)
 
-    def parse(self, text:str):
+    def _parse(self, text:str) -> Iterator[Morpheme]:
         with SpacyAnalyzer.LOCK:
             doc = self.nlp(text)
         for token in doc:
@@ -27,11 +31,34 @@ class SpacyAnalyzer:
                 tag=token.tag_,
             )
 
+    def _filtered_parse(self, text: str, pos_exclude: Iterable[str]) -> Iterator[Morpheme]:
+        with SpacyAnalyzer.LOCK:
+            doc = self.nlp(text)
+        for token in doc:
+
+            if token.pos_ in pos_exclude:
+                continue
+
+            yield Morpheme(
+                lemma=token.lemma_,
+                inflection=token.text,
+                pos=token.pos_,
+                tag=token.tag_,
+            )
+
+    def parse(self, text: str, pos_exclude: Iterable[str] | None = None) -> Iterator[Morpheme]:
+        if pos_exclude is None:
+            return self._parse(text)
+        return self._filtered_parse(text, pos_exclude)
+
+
 
 def get_analyzer() -> SpacyAnalyzer:
     global analyzer
     if analyzer is None:
-        analyzer = SpacyAnalyzer("ja_core_news_sm")
+        # analyzer = SpacyAnalyzer("ja_core_news_sm")
+        # pretrained ja model failing at punctuation and sym, more testing needed
+        analyzer = SpacyAnalyzer("ja", blank=True)
     return analyzer
 
 def install_model(model_name:str) -> None:
