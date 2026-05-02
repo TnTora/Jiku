a0df4820e90ae43858c1d3941ac28c4ccd9772be
TnTora <128092613+TnTora@users.noreply.github.com>
2026-05-02 18:17:46 +0200

    update texthooker options UI (wip)

--- src/frontend/src/routes/texthooker/OptionPanel.svelte
+++ src/frontend/src/routes/texthooker/OptionPanel.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
     import { getTextHookerOptionsContext } from "./context";
     import { clickOutside } from "$lib/utils/clickOutside.js";
+    import SelectOption from "$lib/components/SelectOption.svelte";
     let { onoutsideclick } = $props();
     let options = getTextHookerOptionsContext();
 </script>
 
-<div use:clickOutside={"button[title=Options]"} {onoutsideclick} class="w-[20rem] grid grid-cols-2 items-center justify-between gap-1 px-4 py-4 fixed top-13 right-3 z-9 bg-mist-800/90 border rounded-xl">
+<div use:clickOutside={"button[title=Options]"} {onoutsideclick} class="w-[25rem] max-w-screen grid grid-cols-2 items-center justify-between gap-2 px-4 py-4 fixed top-13 right-3 z-9 bg-mist-800 border-mist-900 border rounded-xl">
+    
+    <h2 class="col-span-2 text-xl font-bold mt-4" style="margin-top:0;">Main</h2>
+    
     <label for="preset">Preset</label>
     <select id="preset" bind:value={options.preset_name}>
         <option value="Default">Default</option>
@@ -14,28 +18,29 @@
     <label for="ws_url">WebSocket URL</label>
     <input id="ws_url" type="text" bind:value={options.websocket_url}>
 
-    <label for="vertical">Vertical</label>
-    <input id="vertical" type="checkbox" bind:checked={options.vertical}>
+    <h2 class="col-span-2 text-xl font-bold mt-4">Layout</h2>
+
+    <!-- <label for="vertical">Vertical</label>
+    <input id="vertical" type="checkbox" bind:checked={options.vertical}> -->
+
+    <div class="col-span-2">
+        <SelectOption bind:selected_value={options.vertical} options={[{name: "Vertical", value: true}, {name: "Horizontal", value: false}]} --height="2rem" />
+    </div>
+
+    <h2 class="col-span-2 text-xl font-bold mt-4">Style</h2>
 
     <label for="font_size">Font Size</label>
-    <div class="flex items-center mx-auto">
-        <button onclick={() => { options.font_size-- }} class="w-10 h-8 pl-1 text-[0.7rem] border rounded-l-full cursor-pointer">A</button>
-        <input id="font_size" type="number" bind:value={options.font_size} class="w-10 h-8 text-center border-t border-b hide-input-spinners" >
-        <button onclick={() => { options.font_size++ }} class="w-10 h-8 pr-1 text-lg border rounded-r-full cursor-pointer">A</button>
+    <div class="flex items-center mx-auto border border-neutral-600 bg-neutral-600 rounded-full">
+        <button onclick={() => { options.font_size-- }} class="w-7 h-7 pl-1 text-[0.7rem] rounded-l-full cursor-pointer bg-neutral-700 active:bg-neutral-600">A</button>
+        <input id="font_size" type="number" bind:value={options.font_size} class="w-9 h-7 text-sm text-center hide-input-spinners z-10">
+        <button onclick={() => { options.font_size++ }} class="w-7 h-7 pr-1 text-[1rem] rounded-r-full cursor-pointer bg-neutral-700 active:bg-neutral-600">A</button>
     </div>
     
 </div>
 
 <style>
-    .hide-input-spinners {
-        -moz-appearance: textfield;
-        appearance: textfield;
+    label, span {
+        width: fit-content;
+        margin-left:0.5rem;
     }
-
-    .hide-input-spinners::-webkit-outer-spin-button,
-    .hide-input-spinners::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-        margin: 0;
-    }
-
 </style>
