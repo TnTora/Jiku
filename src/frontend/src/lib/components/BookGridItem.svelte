f8029d918c551b3f343fa497b7dcbedd34d33c41
TnTora <128092613+TnTora@users.noreply.github.com>
2026-05-08 05:17:32 +0200

    fix books navigation

--- src/frontend/src/lib/components/BookGridItem.svelte
+++ src/frontend/src/lib/components/BookGridItem.svelte
@@ -1,29 +1,30 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
+    // import { goto } from "$app/navigation";
     let { item } = $props();
 
 </script>
 
 
-<div
-    class="book-item border border-neutral-600 active:border-neutral-400 active:border-2 bg-neutral-700 w-full h-full rounded-lg overflow-hidden cursor-pointer"
-    style="background-image: url({item.static_url}/{item.id}/images/{item.thumb});"
-    onclick={() => { goto(`/books/${item.id}`) }}
->
-    <div class="h-full flex flex-col justify-between {item.thumb? "opacity-0 hover:opacity-100": ""} transition-opacity duration-100">
-        <div class="bg-neutral-700/80 text-neutral-300 text-sm h-fit p-1">
-            {item.metadata.title}
+<a href="{`/books/${item.id}`}" class="h-full w-full">
+    <div
+        class="book-item border border-neutral-600 active:border-neutral-400 active:border-2 bg-neutral-700 w-full h-full rounded-lg overflow-hidden cursor-pointer"
+        style="background-image: url({item.static_url}/{item.id}/images/{item.thumb});"
+        // onclick={() => { goto(`/books/${item.id}`) }}
+    >
+        <div class="h-full flex flex-col justify-between {item.thumb? "opacity-0 hover:opacity-100": ""} transition-opacity duration-100">
+            <div class="bg-neutral-700/80 text-neutral-300 text-sm h-fit p-1">
+                {item.metadata.title}
+            </div>
+        
+            <div class="bg-neutral-700/80 text-neutral-300 text-xs h-fit p-1">
+                {#each item.metadata.authors as author}
+                    {author};
+                {/each}
+            </div>
         </div>
-    
-        <div class="bg-neutral-700/80 text-neutral-300 text-xs h-fit p-1">
-            {#each item.metadata.authors as author}
-                {author};
-            {/each}
-        </div>
+        
     </div>
-    
-
-</div>
+</a>
 
 
 <style>
--- src/frontend/src/routes/books/[id]/+page.server.ts
+++ src/frontend/src/routes/books/[id]/+page.server.ts
@@ -1,6 +1,7 @@
 export const load = async ({ params }) => {
     const res = await fetch(`http://127.0.0.1:8000/books/id/${params.id}`);
 	const book = await res.json();
-    console.log(book);
+    // console.log(book);
+    console.log(`loaded book ${params.id}`);
 	return {book};
 };
