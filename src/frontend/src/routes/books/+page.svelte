8d45f46d5c3fe46f67ac6960ae186e16285af352
TnTora <128092613+TnTora@users.noreply.github.com>
2026-05-10 16:28:51 +0200

    update ebooks library

--- src/frontend/src/routes/books/+page.svelte
+++ src/frontend/src/routes/books/+page.svelte
@@ -1,15 +1,112 @@
 <script lang="ts">
     import GridDisplay from "$lib/components/GridDisplay.svelte";
+    import TopBar from "./TopBar.svelte";
     let { data } = $props();
+    let  ascending_order: boolean = $state(false);
+    let item_size_rem: number = $state(12.5);
 </script>
 
 <div class="flex flex-col h-screen">
-    <div class="shrink-0 h-11 w-full bg-neutral-900">Test</div>
+    <div class="shrink-0 h-11 w-full bg-neutral-900">
+        <TopBar/>
+    </div>
 
-    <GridDisplay items={data.books} item_min_w={"12.5rem"}/>
+    <div class="w-full px-4 py-2 flex justify-between items-center gap-x-3 ">
+        <!-- Search Bar -->
+
+        <select
+            id="status"
+            class="px-2 py-1 text-md text-center shrink-2 min-w-13 bg-neutral-900 border-neutral-900 hover:bg-neutral-950 border rounded-lg"
+        >
+            <option>All</option>
+            <option>Reading</option>
+            <option>Completed</option>
+            <option>New</option>
+        </select>
+
+        <input
+            type="text"
+            placeholder="Enter Book Title"
+            class="px-3 py-1 text-sm grow shrink min-w-15 bg-neutral-600 border-neutral-700 hover:border-neutral-500 focus:border-neutral-500 border rounded-md"
+        >
+
+        <button
+            title="Search"
+            class="px-2 py-1 text-sm bg-neutral-900 border-neutral-900 hover:bg-neutral-950 active:bg-[#353535] border rounded-lg"
+        >
+            Search
+        </button>
+    </div>
+
+    <div class="w-full px-4 py-2 flex justify-between items-center gap-x-3 ">
+
+        <div class="ml-1 flex gap-2 justify-between items-center">
+            <!-- Order By -->
+
+            <select
+                id="order-by"
+                class="px-2 py-1 text-sm field-sizing-content text-center bg-neutral-900 border-neutral-900 hover:bg-neutral-950 border rounded-lg"
+            >
+                <option>Title</option>
+                <option>Length</option>
+                <option>Known Unique Words %</option>
+                <option>Known Total Words %</option>
+            </select>
+
+            <button
+                title="Order Toggle"
+                class="px-2 py-1 text-sm bg-neutral-900 border-neutral-900 hover:bg-neutral-950 active:bg-[#353535] border rounded-lg"
+                onclick={() => {ascending_order = !ascending_order}}
+            >
+                {#if ascending_order}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="9" height="16" viewBox="0 0 9 16">
+                        <path fill="currentColor" d="M4.5 14c-.28 0-.5-.22-.5-.5v-9c0-.28.22-.5.5-.5s.5.22.5.5v9c0 .28-.22.5-.5.5" />
+                        <path fill="currentColor" d="M8 7.5a.47.47 0 0 1-.35-.15L4.5 4.2L1.35 7.35c-.2.2-.51.2-.71 0s-.2-.51 0-.71l3.5-3.5c.2-.2.51-.2.71 0l3.5 3.5c.2.2.2.51 0 .71c-.1.1-.23.15-.35.15" />
+                    </svg>
+                {:else}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="9" height="16" viewBox="0 0 9 16">
+                        <path fill="currentColor" d="M4.5 13c-.28 0-.5-.22-.5-.5v-9c0-.28.22-.5.5-.5s.5.22.5.5v9c0 .28-.22.5-.5.5" />
+                        <path fill="currentColor" d="M4.5 14a.47.47 0 0 1-.35-.15l-3.5-3.5c-.2-.2-.2-.51 0-.71s.51-.2.71 0l3.15 3.15l3.15-3.15c.2-.2.51-.2.71 0s.2.51 0 .71l-3.5 3.5c-.1.1-.23.15-.35.15Z" />
+                    </svg>
+                {/if}
+            </button>
+        </div>
+
+        <div class="mr-1 flex gap-1 justify-between items-center">
+            <!-- Item Size -->
+            <span
+                class="mr-2"
+            >
+                Size:
+            </span>
+            <button
+                title="Reduce Size"
+                class="w-6 aspect-square text-sm bg-neutral-900 border-neutral-900 hover:bg-neutral-950 active:bg-[#353535] border rounded-lg"
+                onclick={() => {
+                    if (item_size_rem <= 6.5) { return; }
+                    item_size_rem--;
+                }}
+            >
+                -
+            </button>
+            <button
+                title="Increase Size"
+                class="w-6 aspect-square text-sm bg-neutral-900 border-neutral-900 hover:bg-neutral-950 active:bg-[#353535] border rounded-lg"
+                onclick={() => {item_size_rem++;}}
+            >
+                +
+            </button>
+
+        </div>
+    </div>
+
+    <GridDisplay items={data.books} item_min_w={`${item_size_rem}rem`}/>
 </div>
 
 <style>
 
+input:focus {
+  outline: none;
+}
 
 </style>
---
+++ src/frontend/src/routes/books/TopBar.svelte
@@ -0,0 +1,32 @@
+<script>
+    // let { title, toggleOptions, toggleSidePanel,} = $props();
+    let btn_shared_classes = "hover:text-sky-700 active:text-sky-500 hover:cursor-pointer";
+</script>
+
+<div class="bg-neutral-800 h-11 w-full flex justify-between items-center gap-x-1.5 z-20">
+    <div class="h-full flex justify-start gap-4 ml-4 items-center">
+        <button class="{btn_shared_classes}" title="TOC" onclick={() => {}}>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-8" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M4 17q-.425 0-.712-.288T3 16t.288-.712T4 15h12q.425 0 .713.288T17 16t-.288.713T16 17zm0-4q-.425 0-.712-.288T3 12t.288-.712T4 11h12q.425 0 .713.288T17 12t-.288.713T16 13zm0-4q-.425 0-.712-.288T3 8t.288-.712T4 7h12q.425 0 .713.288T17 8t-.288.713T16 9zm16 8q-.425 0-.712-.288T19 16t.288-.712T20 15t.713.288T21 16t-.288.713T20 17m0-4q-.425 0-.712-.288T19 12t.288-.712T20 11t.713.288T21 12t-.288.713T20 13m0-4q-.425 0-.712-.288T19 8t.288-.712T20 7t.713.288T21 8t-.288.713T20 9" />
+            </svg>
+        </button>
+    </div>
+
+    <div class="h-full flex justify-end gap-4 mr-4 items-center">
+        <button class="{btn_shared_classes}" title="Upload" onclick={() => {}}>
+            <svg class="h-8" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+                    <path d="M20 22H6a2 2 0 0 1-2-2m0 0a2 2 0 0 1 2-2h14V6c0-1.886 0-2.828-.586-3.414S17.886 2 16 2h-6c-2.828 0-4.243 0-5.121.879C4 3.757 4 5.172 4 8z" />
+                    <path d="M19.5 18s-1 .763-1 2s1 2 1 2M9 10s2.21-3 3-3s3 3 3 3m-3-2.5V13" />
+                </g>
+            </svg>
+        </button>
+    </div>
+</div>
+
+
+<style>
+    svg {
+        margin: auto;
+    }
+</style>
