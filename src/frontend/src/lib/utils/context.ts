261aee1f10ce1c5a8e1af2a48f16b93d83decd2f
TnTora <128092613+TnTora@users.noreply.github.com>
2026-05-27 00:19:36 +0200

    frontend: fix missing book cover bad request; remove leftover comments

--- src/frontend/src/lib/utils/context.ts
+++ src/frontend/src/lib/utils/context.ts
@@ -8,16 +8,6 @@ export interface JikuError {
 export const [getJikuErrorsContext, setJikuErrorsContext] = createContext<JikuError[]>();
 
 
-// export interface ConfirmationPopupContext {
-//     show_input_modal: boolean,
-//     use_modal_input: boolean,
-//     input_description: string,
-//     text_input_value: string | null,
-//     text_input_default: string,
-//     modalOk: () => any,
-//     modalCancel: () => any,
-// }
-
 export interface ConfirmationPopupContext {
     show: boolean,
     text: string,
--- src/frontend/src/routes/books/BookGridItem.svelte
+++ src/frontend/src/routes/books/BookGridItem.svelte
@@ -19,7 +19,7 @@
 <a href="{`/books/${item.id}`}" class="h-full w-full">
     <div
         class="book-item border border-neutral-600 active:border-neutral-400 active:border-2 bg-neutral-700 w-full h-full rounded-lg overflow-hidden cursor-pointer"
-        style="background-image: url({item.static_url}/{item.id}/images/{item.thumb});"
+        style="{item.thumb? `background-image: url(${item.static_url}/${item.id}/images/${item.thumb});`: ""}"
     >
         <div class="relative h-full flex flex-col justify-between {item.thumb? "opacity-0 hover:opacity-100": ""} transition-opacity duration-100">
             <div class="bg-neutral-700/80 text-neutral-300 text-sm h-fit p-1">
