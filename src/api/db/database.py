4745326661a6c1306ca903dc39929c31de87b972
TnTora <128092613+TnTora@users.noreply.github.com>
2026-06-27 15:21:05 +0200

    db: add NullPool poolclass
    fix prepared statements when using multiple
    processes in worker.

--- src/api/db/database.py
+++ src/api/db/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.pool import NullPool
 
 import os
 
@@ -14,6 +15,7 @@ if DATABASE_URL is None:
 
 engine = create_engine(
     DATABASE_URL,
+    poolclass=NullPool,
     # echo=True,
 )
 
