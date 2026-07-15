d70dac3e1b5e3b58d6d0da76a7e3f5fafd25fdb9
TnTora <128092613+TnTora@users.noreply.github.com>
2026-07-15 16:46:39 +0200

    fix texthooker status

--- src/frontend/src/lib/components/TexthookerLine.svelte
+++ src/frontend/src/lib/components/TexthookerLine.svelte
@@ -8,7 +8,7 @@
 <div class="relative flex items-center">
     <p class="my-1 py-1 px-5 whitespace-pre-wrap" style="font-size: {options.font_size}px;">
         {#each line.tokens as word}
-            {#if word.inflection in status_map}
+            {#if word.lemma in status_map}
                 <span class="relative status-underline {(status_map[word.lemma] == 1)? "known": "new"}">{word.inflection}</span>
             {:else if (word.pos !== "PUNCT" && word.pos !== "SPACE" && word.pos !== "X")}
                 <span class="relative status-underline unknown">{word.inflection}</span>
