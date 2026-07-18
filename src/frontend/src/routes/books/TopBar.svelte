f21c164564cd542373e58249df22f27b0f4b44ad
TnTora <128092613+TnTora@users.noreply.github.com>
2026-07-18 17:27:59 +0200

    frontend: add home button

--- src/frontend/src/routes/books/TopBar.svelte
+++ src/frontend/src/routes/books/TopBar.svelte
@@ -39,6 +39,7 @@
 
 <div class="bg-neutral-800 h-11 w-full flex justify-between items-center gap-x-1.5 z-20">
     <div class="h-full flex justify-start gap-4 ml-4 items-center">
+        
         {#if !show_side_panel}
             <button class="{btn_shared_classes}" title="Open Sidepanel" onclick={toggleSidePanel}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-8" viewBox="0 0 24 24">
@@ -51,7 +52,14 @@
                     <path fill="currentColor" d="M3.64 2.27L7.5 6.13l3.84-3.84A.92.92 0 0 1 12 2a1 1 0 0 1 1 1a.9.9 0 0 1-.27.66L8.84 7.5l3.89 3.89A.9.9 0 0 1 13 12a1 1 0 0 1-1 1a.92.92 0 0 1-.69-.27L7.5 8.87l-3.85 3.85A.92.92 0 0 1 3 13a1 1 0 0 1-1-1a.9.9 0 0 1 .27-.66L6.16 7.5L2.27 3.61A.9.9 0 0 1 2 3a1 1 0 0 1 1-1c.24.003.47.1.64.27" />
                 </svg>
             </button>
-    {/if}
+        {/if}
+
+        <a class="{btn_shared_classes}" title="Home" href="/">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24">
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path fill="currentColor" d="m12 5.69l5 4.5V18h-2v-6H9v6H7v-7.81zM12 3L2 12h3v8h6v-6h2v6h6v-8h3" />
+            </svg>
+        </a>
     </div>
 
     <div class="h-full flex justify-end gap-4 mr-4 items-center">
--- src/frontend/src/routes/texthooker/TopBar.svelte
+++ src/frontend/src/routes/texthooker/TopBar.svelte
@@ -6,8 +6,19 @@
 </script>
 
 <div class="fixed top-0 right-0 bg-mist-800/90 border-l border-b border-mist-700/90 h-10 px-3 rounded-bl-lg flex items-center z-10">
-    <button class="{btn_shared_classes} {ws_conn_color} font-semibold" onclick={toggleWebSocket} title="Toggle WebSocket">WS</button>
-    <button class="{btn_shared_classes}" onclick={clearAllLines} title="Clear all lines">
+    <button
+        class="{btn_shared_classes} {ws_conn_color} font-semibold"
+        onclick={toggleWebSocket}
+        title="Toggle WebSocket"
+    >
+        WS
+    </button>
+
+    <button
+        class="{btn_shared_classes}"
+        onclick={clearAllLines}
+        title="Clear all lines"
+    >
         <svg xmlns="http://www.w3.org/2000/svg" class="h-7" viewBox="0 0 24 24">
             <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="1.5">
                 <path stroke-linejoin="round" d="m15 18.5l5-5m0 5l-5-5" />
@@ -15,7 +26,19 @@
             </g>
         </svg>
     </button>
-    <button class="{btn_shared_classes}" onclick={toggleOptions} title="Options">
+
+    <a class="{btn_shared_classes}" title="Home" href="/">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-7" viewBox="0 0 24 24">
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path fill="currentColor" d="m12 5.69l5 4.5V18h-2v-6H9v6H7v-7.81zM12 3L2 12h3v8h6v-6h2v6h6v-8h3" />
+        </svg>
+    </a>
+
+    <button 
+        class="{btn_shared_classes}"
+        onclick={toggleOptions}
+        title="Options"
+    >
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5" viewBox="0 0 512 512">
             <path fill="currentColor" d="M495.9 166.6c3.2 8.7.5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4l-55.6 17.8c-8.8 2.8-18.6.3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4c-1.1-8.4-1.7-16.9-1.7-25.5s.6-17.1 1.7-25.4l-43.3-39.4c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160a80 80 0 1 0 0 160" />
         </svg>
