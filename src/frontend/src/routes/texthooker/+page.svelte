0c62a266d3bc93ea4e9815cff9a488b0e2baef25
TnTora <128092613+TnTora@users.noreply.github.com>
2026-07-30 00:26:58 +0200

    frontend: update texthooker virtual list
    - fix scrolling up jitter
    - add scrollToIndex function
    - add scrollToLast button in TopBar

--- src/frontend/src/routes/texthooker/+page.svelte
+++ src/frontend/src/routes/texthooker/+page.svelte
@@ -141,17 +141,6 @@
         () => { return { top: text_container.scrollHeight } }
     );
 
-    async function scrollToLast() {
-        text_container?.scrollTo(getLastScrollPos());
-        await tick();
-        if (end < new_lines.length) {
-            end = new_lines.length;
-            await tick();
-        }
-        text_container?.scrollTo(getLastScrollPos());
-    }
-
-
     async function processNewLine(new_line: string) {
         try {
             const res = await api_fetch("texthooker/new_line", {
@@ -176,7 +165,6 @@
         } catch (error) {
             throw error;
         }
-
     }
 
     async function deleteLine(line_id: number) {
@@ -292,6 +280,7 @@
 
     let length_map: number[] = [];
     let seen_items_count = 0;
+    let guessed_item_size = options.font_size * options.line_height;
     let average_item_size = $state(options.font_size * options.line_height);
     let estimated_total_size = $derived(average_item_size*new_lines.length);
 
@@ -301,11 +290,7 @@
 
     let before_spacing = $state(0);
     let rendered_length = $state(0);
-    let after_spacing: number = $derived(
-        (end < new_lines.length)?
-        estimated_total_size - before_spacing - rendered_length:
-        0
-    );
+    let after_spacing: number = $derived((new_lines.length-end)*average_item_size);
 
     let visible_lines: (LineBase|TmpLine)[] = $derived.by(() => {
         return new_lines.slice(start, end);
@@ -324,7 +309,7 @@
     );
 
     let rafHandler = createRafHandler();
-    let oldScrollPosition = 0;
+    let old_scroll_position = 0;
 
     // $effect(() => {
     //     // $inspect(visible_lines);
@@ -338,16 +323,22 @@
     });
 
     $effect(() => {
+        let new_content_length = 0;
         for (let j = 0; j < visible_html_elements.length; j++) {
             if (!visible_html_elements[j]) { continue; }
 
             if (length_map[start + j] === undefined) {
-                length_map[start + j] = getOffsetLength(visible_html_elements[j]);
-                average_item_size = ((average_item_size*seen_items_count) + getOffsetLength(visible_html_elements[j])) / (seen_items_count + 1);
+                const item_size = getOffsetLength(visible_html_elements[j])
+                length_map[start + j] = item_size;
+                average_item_size = ((average_item_size*seen_items_count) + item_size) / (seen_items_count + 1);
                 seen_items_count++;
+                // console.log("seen_items_count", seen_items_count);
+                // console.log("estimated_total_size", estimated_total_size);
             }
+
+            new_content_length += length_map[start + j];
 		}
-        // console.log(length_map);
+        rendered_length = new_content_length;
     });
 
     $effect(() => { refreshVisibles(new_lines, text_container_offlength); });
@@ -355,7 +346,7 @@
     $effect(() => {
         if (text_container_ortogonal_offlength || options.vertical){
             length_map.length = 0;
-            console.log(length_map);
+            // console.log(length_map);
         }
     });
 
@@ -400,42 +391,45 @@
 	}
 
     async function handle_scroll() {
-        const scrollPos = getScrollPosition(text_container);
+        let scrollPos = getScrollPosition(text_container);
+
+        // console.log("scrollPos: ", scrollPos);
 
         // avoid recalculating for small scrolling changes
-        if (Math.abs(scrollPos - oldScrollPosition) < average_item_size*0.5) {
+        if (Math.abs(scrollPos - old_scroll_position) < guessed_item_size*0.75) {
             return;
         }
 
-        oldScrollPosition = scrollPos;
+        old_scroll_position = scrollPos;
+        const old_start = start;
+        const old_spacing = before_spacing;
 
-        // console.log("scrollTop handle_scroll: ", scrollTop);
+        // console.log("scrollPos handle_scroll: ", scrollPos);
 
 		let i = 0;
 		let y = 0;
-        let content_height = 0;
+        let start_buffer = new_lines.length;
+        let y_buffer;
+        let content_length = 0;
 
         await tick();
 
+        // find first visible item
 		while (i < new_lines.length) {
-			const item_height = length_map[i] || average_item_size;
+			const item_height = length_map[i] || guessed_item_size;
 			if (y + item_height > scrollPos) {
 
                 // apply buffer
-                let start_buffer = i;
-                let y_buffer = y;
+                start_buffer = i;
+                y_buffer = y;
                 let j = 1;
 
                 while (j <= buffer && start_buffer > 0) {
                     start_buffer--;
-                    y_buffer = y_buffer - (length_map[i-j] || average_item_size);
+                    y_buffer -= (length_map[i-j] || guessed_item_size);
                     j++;
                 }
 
-                start = start_buffer;
-                before_spacing = y_buffer;
-
-                content_height += item_height;
 				break;
 
 			}
@@ -444,9 +438,9 @@
 			i += 1;
 		}
 
+        // find last item 
 		while (i < new_lines.length) {
             const item_height = length_map[i] || average_item_size;
-            content_height += item_height;
 			y += item_height;
 			i += 1;
 
@@ -456,24 +450,81 @@
         let end_buffer = i;
         let j = 0;
 
+        // apply buffer to end
         while (j < buffer && end_buffer < new_lines.length) {
             end_buffer++;
-            content_height += (length_map[i+j] || average_item_size);
             j++;
         }
 
-		// end = Math.min(i+buffer, new_lines.length);
+        if (y_buffer === undefined) { return; }
+
+        // update states
+        start = start_buffer;
+        before_spacing = y_buffer;
         end = end_buffer;
-        rendered_length = content_height;
+
+        await tick(); // wait for items rendering
+
+        if (start < old_start) {
+            // correct scroll position when scrolling up without a known offset
+            // due to rendered items size being different from the estimated one
+
+            let actual_length = 0;
+            let estimated_length = old_spacing-before_spacing;
+
+            for(let i = start; i < old_start; i++) {
+                if (visible_html_elements[i-start]) {
+                    actual_length += getOffsetLength(visible_html_elements[i-start]);
+                }
+            }
+
+            const d = actual_length - estimated_length;
+
+            if (!d) { return; }
+
+            const target = options.vertical? 
+                {left: -scrollPos - d} :
+                {top: scrollPos + d} ;
+
+            // console.log(old_start, start);
+            // console.log("before_spacing", before_spacing, "old_spacing", old_spacing);
+            // console.log(actual_length, estimated_length, actual_length - estimated_length);
+            // console.log("target", target);
+
+            text_container.scrollTo(target);
+        }
 	}
 
-    // onMount(() => {
-    //     visible_html_elements = text_container?.getElementsByTagName("div");;
-    // });
+    async function scrollToIndex(index: number) {
+        if (index < 0) {
+            index = Math.max(0, new_lines.length + index);
+        } else if (index >= new_lines.length) {
+            index = new_lines.length - 1;
+        }
+
+		let y = 0;
+
+        for (let i=0; i < index; i++) {
+            y += length_map[i] || guessed_item_size;
+        }
+
+        const target = options.vertical? {left: -y}: {top: y};
+        console.log("index", index, "target", target);
+
+        text_container.scrollTo(target);
+	}
+
+    function scheduled_scroll() {
+        rafHandler(handle_scroll);
+    }
+
+    function scrollToLast() {
+        scrollToIndex(-1);
+    }
 
 </script>
 
-<TopBar {toggleWebSocket} {ws_connected} {clearAllLines} toggleOptions={() => {show_options = !show_options}}/>
+<TopBar {toggleWebSocket} {ws_connected} {clearAllLines} {scrollToLast} toggleOptions={() => {show_options = !show_options}}/>
 
 {#if show_options}
     <OptionPanel presets={data.presets} bind:preset_name={preset_name} onoutsideclick={() => {show_options = false}}/>
@@ -509,12 +560,10 @@
     id="texthooker-container"
     class="relative pt-10 pb-6 w-full h-screen overflow-auto {options.vertical? "vert-rl pl-5 pr-2": ""}"
     style="line-height: {options.line_height};"
-    onscroll={() => {
-        rafHandler(handle_scroll);
-    }}
+    onscroll={scheduled_scroll}
 >
     <div style="{options.vertical? "padding-right": "padding-top"}: {before_spacing}px; {options.vertical? "padding-left":"padding-bottom"}: {after_spacing}px;">
-        {#each visible_lines as line, index}
+        {#each visible_lines as line, index }
             <div bind:this={visible_html_elements[index]}>
                 {#if isTmpLine(line)}
                     <!-- line added during current session -->
